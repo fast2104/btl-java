da152cd51fc2faa9a30e23d9ad19d2c94b917756
Jojo <164637156+JoJo9805@users.noreply.github.com>
2025-10-23 15:21:21 +0700

    Update main.java

--- java/main.java
+++ java/main.java
@@ -1,152 +1,71 @@
-import java.util.*;
-
+import java.util.Scanner;
 public class main {
-
-    // ====== Lớp SanPham ======
-    static class SanPham {
-        private String maSanPham;
-        private String tenSanPham;
-        private String loaiSanPham;
-        private String nguonGoc;
-        private String moTa;
-        private String donVi;
-        private long giaBan;
-        private long giaMua;
-        private int tonKho;
-        private int daBan;
-
-        public void input(Scanner sc) {
-            System.out.print("Nhập mã sản phẩm: ");
-            maSanPham = sc.nextLine();
-            System.out.print("Nhập tên sản phẩm: ");
-            tenSanPham = sc.nextLine();
-            System.out.print("Nhập loại sản phẩm: ");
-            loaiSanPham = sc.nextLine();
-            System.out.print("Nhập nguồn gốc: ");
-            nguonGoc = sc.nextLine();
-            System.out.print("Nhập mô tả: ");
-            moTa = sc.nextLine();
-            System.out.print("Nhập đơn vị: ");
-            donVi = sc.nextLine();
-            System.out.print("Nhập giá bán: ");
-            giaBan = sc.nextLong();
-            System.out.print("Nhập giá mua: ");
-            giaMua = sc.nextLong();
-            System.out.print("Nhập tồn kho: ");
-            tonKho = sc.nextInt();
-            System.out.print("Nhập đã bán: ");
-            daBan = sc.nextInt();
-            sc.nextLine();
-        }
-
-        public void output() {
-            System.out.println("Mã SP: " + maSanPham + " | Tên: " + tenSanPham +
-                    " | Loại: " + loaiSanPham + " | Giá bán: " + giaBan +
-                    " | Giá mua: " + giaMua + " | Tồn kho: " + tonKho + " | Đã bán: " + daBan);
-        }
-    }
-
-    // ====== Lớp NhanVien ======
-    static class NhanVien {
-        private String hoTen, cccd, diaChi, soDienThoai, gioiTinh, vaiTro, trangThai, user, password, quyenHan, maNhanVien;
-        private Date DoB;
-        private long luong;
-
-        public void input(Scanner sc) {
-            System.out.print("Nhập họ tên: ");
-            hoTen = sc.nextLine();
-            System.out.print("Nhập CCCD: ");
-            cccd = sc.nextLine();
-            System.out.print("Nhập địa chỉ: ");
-            diaChi = sc.nextLine();
-            System.out.print("Nhập số điện thoại: ");
-            soDienThoai = sc.nextLine();
-            System.out.print("Nhập giới tính: ");
-            gioiTinh = sc.nextLine();
-            System.out.print("Nhập vai trò: ");
-            vaiTro = sc.nextLine();
-            System.out.print("Nhập trạng thái: ");
-            trangThai = sc.nextLine();
-            System.out.print("Nhập username: ");
-            user = sc.nextLine();
-            System.out.print("Nhập password: ");
-            password = sc.nextLine();
-            System.out.print("Nhập quyền hạn: ");
-            quyenHan = sc.nextLine();
-            System.out.print("Nhập mã nhân viên: ");
-            maNhanVien = sc.nextLine();
-            System.out.print("Nhập lương: ");
-            luong = sc.nextLong();
-            sc.nextLine();
-        }
-
-        public void output() {
-            System.out.println("Nhân viên: " + hoTen + " | CCCD: " + cccd +
-                    " | SDT: " + soDienThoai + " | Vai trò: " + vaiTro + " | Lương: " + luong);
-        }
-    }
-
-    // ====== Lớp QuanTriVien kế thừa NhanVien ======
-    static class QuanTriVien extends NhanVien {
-        private ArrayList<NhanVien> dsNhanVien = new ArrayList<>();
-
-        public void themNhanVien(NhanVien nv) {
-            dsNhanVien.add(nv);
-        }
-
-        public void xoaNhanVien(String ma) {
-            dsNhanVien.removeIf(nv -> nv.maNhanVien.equals(ma));
-        }
-
-        public void suaNhanVien(String ma, String tenMoi) {
-            for (NhanVien nv : dsNhanVien) {
-                if (nv.maNhanVien.equals(ma)) {
-                    nv.hoTen = tenMoi;
-                }
-            }
-        }
-
-        public NhanVien timKiemNhanVien(String ma) {
-            for (NhanVien nv : dsNhanVien) {
-                if (nv.maNhanVien.equals(ma)) return nv;
-            }
-            return null;
-        }
-    }
-
-    // ====== Lớp DonViCungCap ======
-    static class DonViCungCap {
-        private String maDonVi, tenDonVi, diaChi;
-        private Vector<String> matHang = new Vector<>();
-    }
-
-    // ====== Lớp ChiTietPhieuNhap ======
-    static class ChiTietPhieuNhap {
-        private SanPham sanPham;
-        private int soLuong;
-        private long donGia;
-    }
-
-    // ====== Lớp PhieuNhapHang ======
-    static class PhieuNhapHang {
-        private int maPhieu;
-        private Date ngayNhap;
-        private DonViCungCap nhaCungCap;
-        private long tongTien;
-        private NhanVien nguoiLapPhieu;
-        private Vector<ChiTietPhieuNhap> chiTietPhieuNhap = new Vector<>();
-    }
-
-    // ====== Main để test ======
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        QuanTriVien qtv = new QuanTriVien();
+        int chon;
+
+        do {
+            System.out.println("\n  MENU CHINH");
+            System.out.println("1. Quan ly nhan vien");
+            System.out.println("2. Nhap thong tin QL, SP, DVCC, PhieuNhapHang");
+            System.out.println("0. Thoat");
+            System.out.print("Chon: ");
+            chon = sc.nextInt();
+            sc.nextLine();
+
+            switch (chon) {
+                case 1 -> bang1(qtv);
+                case 2 -> bang2(sc, qtv);
+                case 0 -> System.out.println("Thoat chuong trinh.");
+                default -> System.out.println("Lua chon khong hop le.");
+            }
+        } while (chon != 0);
+    }
+
+    private static void bang1(QuanTriVien qtv) {
+        Scanner sc = new Scanner(System.in);
+        int c;
+        do {
+            System.out.println("\n  MENU QUAN LY");
+            System.out.println("1. Them nhan vien");
+            System.out.println("2. Xoa nhan vien");
+            System.out.println("3. Hien thi tat ca");
+            System.out.println("0. Quay lai");
+            System.out.print("Chon: ");
+            c = sc.nextInt();
+            sc.nextLine();
+
+            switch (c) {
+                case 1 -> qtv.themNhanVien();
+                case 2 -> qtv.xoaNhanVien();
+                case 3 -> qtv.hienThiTatCaNhanVien();
+            }
+        } while (c != 0);
+    }
+
+    private static void bang2(Scanner sc, QuanTriVien qtv) {
+        System.out.println("NHAP THONG TIN QUAN LY");
+        qtv.input(sc);
+        System.out.println("THONG TIN QUAN LY: ");
+        qtv.output();
 
         SanPham sp = new SanPham();
+        System.out.println("\nNHAP SAN PHAM:");
         sp.input(sc);
+        System.out.println("THONG TIN SAN PHAM:");
         sp.output();
 
-        NhanVien nv = new NhanVien();
-        nv.input(sc);
-        nv.output();
+        DonViCungCap dv = new DonViCungCap();
+        System.out.println("\nNHAP DV CUNG CAP:");
+        dv.input(sc);
+        System.out.println("THONG TIN DVCC:");
+        dv.output();
+
+        PhieuNhapHang phieu = new PhieuNhapHang();
+        System.out.println("\nNHAP PHIEU NHAP HANG:");
+        phieu.input(sc);
+        System.out.println("THONG TIN PHIEU:");
+        phieu.output();
     }
 }
