99cee43e9a74976a70ad9a03795852816de45a75
Jojo <164637156+JoJo9805@users.noreply.github.com>
2025-11-06 16:54:12 +0700

    Update SanPham.java

--- java/SanPham.java
+++ java/SanPham.java
@@ -1,50 +1,59 @@
 import java.util.*;
 import java.util.Scanner;
-public class NhanVien extends connguoi {
+public class SanPham implements Ihanghoa {
+    private String maSanPham;
+    private String tenSanPham;
+    private String loaiSanpham;
+    private String nguongoc;
+    private String moTa;
+    private String donvi;
+    private int giaBan;
+    private int giaMua;
+    private int tonKho;
+    private int daBan;
+    
     @Override
     public void input(Scanner sc) {
-        System.out.print("Nhap ho ten: ");
-        hoTen = sc.nextLine();
-        System.out.print("Nhap CCCD: ");
-        cccd = sc.nextLine();
-        System.out.print("Nhap dia chi: ");
-        diaChi = sc.nextLine();
-        System.out.print("Nhap so dien thoai: ");
-        soDienThoai = sc.nextLine();
-        System.out.print("Nhap ngay sinh: ");
-        doB = sc.nextLine();
-        System.out.print("Nhap gioi tinh: ");
-        gioiTinh = sc.nextLine();
-        System.out.print("Nhap vai tro: ");
-        vaitro = sc.nextLine();
-        System.out.print("Nhap trang thai: ");
-        trangthai = sc.nextLine();
-        System.out.print("Nhap user: ");
-        user = sc.nextLine();
-        System.out.print("Nhap password: ");
-        password = sc.nextLine();
-        System.out.print("Nhap quyen han: ");
-        quyenhan = sc.nextLine();
-        System.out.print("Nhap ma nhan vien: ");
-        maNhanVien = sc.nextLine();
-        System.out.print("Nhap luong: ");
-        luong = sc.nextLong();
+        System.out.print("Nhap ma SP: ");
+        maSanPham = sc.nextLine();
+        sc.next();
+        System.out.print("Nhap ten SP: ");
+        tenSanPham = sc.nextLine();
+        sc.next();
+        System.out.print("nhap loai san pham: ");
+        loaiSanpham = sc.nextLine();
+        sc.next();
+        System.out.print("nhap nguon goc: ");
+        nguongoc = sc.nextLine();
+        sc.next();
+        System.out.print("nhap mo ta: ");
+        moTa = sc.nextLine();
+        sc.next();
+        System.out.print("nhap don vi: ");
+        donvi = sc.nextLine();
+        sc.next();
+        System.out.print("Nhap gia ban: ");
+        giaBan = sc.nextInt();
+        System.out.print("Nhap gia mua: ");
+        giaMua = sc.nextInt();
+        System.out.print("Nhap ton kho: ");
+        tonKho = sc.nextInt();
+        System.out.print("Nhap da ban: ");
+        daBan = sc.nextInt();
+        sc.nextLine();
     }
-
+    
     @Override
     public void output() {
-        System.out.println("Ho ten: " + hoTen);
-        System.out.println("CCCD: " + cccd);
-        System.out.println("Dia chi: " + diaChi);
-        System.out.println("SDT: " + soDienThoai);
-        System.out.println("Ngay sinh: " + doB);
-        System.out.println("Gioi tinh: " + gioiTinh);
-        System.out.println("vai tro: " + vaitro);
-        System.out.println("trang thai: " + trangthai);
-        System.out.println("user: " + user);
-        System.out.println("password: " + password);
-        System.out.println("quyen han: " + quyenhan);
-        System.out.println("Ma nhan vien: " + maNhanVien);
-        System.out.println("Luong: " + luong);
+        System.out.println("Ma SP: " + maSanPham);
+        System.out.println("Ten SP: " + tenSanPham);
+        System.out.println("loai SP: " + loaiSanpham);
+        System.out.println("nguon goc: " + nguongoc);
+        System.out.println("mo ta: " + moTa);
+        System.out.println("don vi: " + donvi);
+        System.out.println("Gia ban: " + giaBan);
+        System.out.println("Gia mua: " + giaMua);
+        System.out.println("Ton kho: " + tonKho);
+        System.out.println("Da ban: " + daBan);
     }
 }
